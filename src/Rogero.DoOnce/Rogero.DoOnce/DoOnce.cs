2fbdfabb28c17a1c89135da853074ec2d58817e6
Paul Rogero <progero@gmail.com>
2015-08-03 10:19:34 -0400

    Initial commit.

---
+++ src/Rogero.DoOnce/Rogero.DoOnce/DoOnce.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading;
+
+namespace Rogero.DoOnce
+{
+    /// <summary>
+    ///     A thread-safe class to ensure an action is only performed one time.
+    /// </summary>
+    public class DoOnce
+    {
+        long _isDone = 0; //0 is false, 1 is true.
+
+        /// <summary>
+        ///     Runs the provided action only if this method has never been called before.
+        /// </summary>
+        /// <param name="action">The action to perform.</param>
+        /// <param name="resetOnException">
+        ///     If true, then if an exception occurs while running the action then the status of this
+        ///     gate will be as if the action was never performed
+        /// </param>
+        /// <returns>True if the action was done, false if the action was not done.</returns>
+        public bool Ensure(Action action, bool resetOnException)
+        {
+            var shouldDo = ShouldDo();
+            if (shouldDo)
+            {
+                PerformAction(action, resetOnException);
+            }
+            return shouldDo;
+        }
+
+        void PerformAction(Action action, bool resetOnException)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception exception)
+            {
+                if (resetOnException)
+                {
+                    Reset();
+                }
+                ExceptionHandler.Handle(exception);
+            }
+        }
+
+        /// <summary>
+        ///     Returns true if the method has never been called before, returns false if the method has been called.
+        /// </summary>
+        /// <returns></returns>
+        bool ShouldDo()
+        {
+            if (Interlocked.Exchange(ref _isDone, 1) == 0)
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        ///     Resets this object as if the action has never been performed.
+        /// </summary>
+        void Reset()
+        {
+            Interlocked.Exchange(ref _isDone, 0);
+        }
+
+        /// <summary>
+        ///     Returns true if the action still needs to be performed, false otherwise.
+        /// </summary>
+        public bool Done => Interlocked.Read(ref _isDone) == 1;
+    }
+}
---
+++ src/Rogero.DoOnce/Rogero.DoOnce/ExceptionHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Rogero.DoOnce
+{
+    /// <summary>
+    /// A class that allows the user to provide an Action to be performed when an exception is raised in this component.
+    /// </summary>
+    public static class ExceptionHandler
+    {
+        internal static void Handle(Exception exception)
+        {
+            if (ExceptionLogger == null)
+            {
+                Console.WriteLine(exception);
+            }
+            else
+            {
+                ExceptionLogger(exception);
+            }
+        }
+
+        /// <summary>
+        /// The method called when an exception is raised in this component.
+        /// </summary>
+        public static Action<Exception> ExceptionLogger { get; set; } 
+    }
+}
